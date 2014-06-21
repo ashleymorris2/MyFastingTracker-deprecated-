7079a424de43d6e7361e59984d589b9e07b28dea
ashm87 <amorris8@uclan.ac.uk>
2014-06-21 22:51:22 +0100

    SeekBar is working

--- app/src/main/java/com/avaygo/myfastingtracker/frontpage.java
+++ app/src/main/java/com/avaygo/myfastingtracker/frontpage.java
@@ -6,37 +6,68 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
 
 
 public class frontpage extends Activity {
 
     Button BStartToggle;
-    TextView TStatusText, TCurrentTime, TEndTime;
+    TextView TStatusText, TCurrentTime, TEndTime, TSeekVal;
+    SeekBar timeSeek;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
+        //Load the layout
 
         //Find View Elements
         TStatusText = (TextView) findViewById(R.id.status_text);
         TCurrentTime = (TextView) findViewById(R.id.current_time);
         TEndTime = (TextView) findViewById(R.id.end_time);
+        TSeekVal = (TextView) findViewById(R.id.seekVal);
+
+        timeSeek = (SeekBar) findViewById(R.id.timeSeek);
+
+        timeSeek.setOnSeekBarChangeListener(timeSeek_listener);
 
         BStartToggle = (Button) findViewById(R.id.start_toggle);
         BStartToggle.setOnClickListener(BStartToggle_OnClickListener);
+
     }
+
 
     //On click listener for BStartToggle
     final View.OnClickListener BStartToggle_OnClickListener = new View.OnClickListener() {
         public void onClick(View v) {
-
             //place holder
             TStatusText.setText("Fasting Started");
+        }
+    };
+
+    final SeekBar.OnSeekBarChangeListener timeSeek_listener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            TSeekVal.setText("Hours: " +progress);
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
 
         }
     };
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
