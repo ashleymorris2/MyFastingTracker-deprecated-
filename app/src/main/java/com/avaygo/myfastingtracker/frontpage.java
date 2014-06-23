205ca803a0a0aa74c2dce4a9b1b58d4f4bfad22f
ashm87 <amorris8@uclan.ac.uk>
2014-06-23 16:48:46 +0100

    Timer is Working

--- app/src/main/java/com/avaygo/myfastingtracker/frontpage.java
+++ app/src/main/java/com/avaygo/myfastingtracker/frontpage.java
@@ -9,13 +9,19 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class frontpage extends Activity {
 
     Button BStartToggle;
-    TextView TStatusText, TCurrentTime, TEndTime, TSeekVal;
+    TextView TStatusText, TCurrentTime, TEndTime, TSeekVal, TCurrentClock;
     SeekBar timeSeek;
+
+    Thread myThread = null;
+    Runnable myRunnableThread = new CountDownRunner();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,14 +30,18 @@ public class frontpage extends Activity {
         setContentView(R.layout.activity_frontpage);
         //Load the layout
 
+        myThread= new Thread(myRunnableThread);
+        myThread.start();
+
         //Find View Elements
         TStatusText = (TextView) findViewById(R.id.status_text);
         TCurrentTime = (TextView) findViewById(R.id.current_time);
+        TCurrentClock = (TextView) findViewById(R.id.clock_text);
+
         TEndTime = (TextView) findViewById(R.id.end_time);
         TSeekVal = (TextView) findViewById(R.id.seekVal);
 
         timeSeek = (SeekBar) findViewById(R.id.timeSeek);
-
         timeSeek.setOnSeekBarChangeListener(timeSeek_listener);
 
         BStartToggle = (Button) findViewById(R.id.start_toggle);
@@ -39,21 +49,28 @@ public class frontpage extends Activity {
 
     }
 
-
     //On click listener for BStartToggle
-    final View.OnClickListener BStartToggle_OnClickListener = new View.OnClickListener() {
+    final View.OnClickListener BStartToggle_OnClickListener = new View.OnClickListener(){
         public void onClick(View v) {
             //place holder
-            TStatusText.setText("Fasting Started");
+            TStatusText.setText("Started");
+            BStartToggle.setText("Breakfast");
         }
     };
 
+    //Seek bar listener for timeSeek
     final SeekBar.OnSeekBarChangeListener timeSeek_listener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-            TSeekVal.setText("Hours: " +progress);
+            int seekValue = progress + 1;
 
+            if (seekValue == 1) {
+                TSeekVal.setText("Duration: " + seekValue + " hour");
+            }
+            else{
+                TSeekVal.setText("Duration: " + seekValue + " hours");
+            }
         }
 
         @Override
@@ -67,7 +84,45 @@ public class frontpage extends Activity {
         }
     };
 
+    public void upDateTime() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try{
+                    Calendar myCalendar = Calendar.getInstance();
+                    SimpleDateFormat timeFormat;
+                    timeFormat = new SimpleDateFormat("HH:mm:ss");
+                    TCurrentClock.setText(timeFormat.format(myCalendar.getTime()));
+                }
+                catch (Exception e) {
 
+
+                }
+            }
+        });
+    }
+
+
+    class CountDownRunner implements Runnable{
+        //This thread runs every 1 second in the background which updates the text view in the doWork() method.
+        // @Override
+        public void run() {
+            while(!Thread.currentThread().isInterrupted()){
+                try {
+                    upDateTime();
+                    Thread.sleep(1000); // Pause of 1 Second
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }catch(Exception e){
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        myThread.interrupt();
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
