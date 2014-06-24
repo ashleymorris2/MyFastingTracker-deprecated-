2deadc2f703818123fad09c55b4f9e1e059414a6
ashm87 <amorris8@uclan.ac.uk>
2014-06-24 16:35:04 +0100

    The future timer is now working as intended.

--- app/src/main/java/com/avaygo/myfastingtracker/frontpage.java
+++ app/src/main/java/com/avaygo/myfastingtracker/frontpage.java
@@ -12,25 +12,32 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-
 public class frontpage extends Activity {
 
+    //UI Elements:
     Button BStartToggle;
-    TextView TStatusText, TCurrentTime, TEndTime, TSeekVal, TCurrentClock;
+    TextView TStatusText, TCurrentTime, TEndTime, TSeekVal, TCurrentClock, TEndClock, TEndHour;
+    TextView TDayText;
     SeekBar timeSeek;
 
+    //Threads and Runnables.
     Thread myThread = null;
     Runnable myRunnableThread = new CountDownRunner();
 
+    //Calendars and time formatting.
+    Calendar currentCalendar, futureCalendar;
+    SimpleDateFormat currentTimeFormat = new SimpleDateFormat("HH:mm:ss");//Current time
+    SimpleDateFormat futureHourFormat = new SimpleDateFormat("HH:");//The hour for the future clock.
+    SimpleDateFormat futureMinuteFormat = new SimpleDateFormat("mm:ss");//The minutes for the future clock
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frontpage);
-        //Load the layout
+        setContentView(R.layout.activity_frontpage);//Load the layout
 
-        myThread= new Thread(myRunnableThread);
+
+        myThread= new Thread(myRunnableThread);//New thread to run the timer separately so that the UI doesn't get held up.
         myThread.start();
 
         //Find View Elements
@@ -38,8 +45,11 @@ public class frontpage extends Activity {
         TCurrentTime = (TextView) findViewById(R.id.current_time);
         TCurrentClock = (TextView) findViewById(R.id.clock_text);
 
+        TEndClock = (TextView) findViewById(R.id.endclock_text);
+        TEndHour = (TextView) findViewById(R.id.dynamicHour);
         TEndTime = (TextView) findViewById(R.id.end_time);
         TSeekVal = (TextView) findViewById(R.id.seekVal);
+        TDayText = (TextView) findViewById(R.id.dayText);
 
         timeSeek = (SeekBar) findViewById(R.id.timeSeek);
         timeSeek.setOnSeekBarChangeListener(timeSeek_listener);
@@ -47,14 +57,22 @@ public class frontpage extends Activity {
         BStartToggle = (Button) findViewById(R.id.start_toggle);
         BStartToggle.setOnClickListener(BStartToggle_OnClickListener);
 
+        //Starts the clocks.
+        futureCalendar = Calendar.getInstance();
+        currentCalendar = Calendar.getInstance();
+
+        futureCalendar.add(Calendar.HOUR_OF_DAY, 1); //Sets the future clock to be at least an hour ahead.
+        TEndHour.setText(futureHourFormat.format(futureCalendar.getTime()));
+
     }
 
     //On click listener for BStartToggle
-    final View.OnClickListener BStartToggle_OnClickListener = new View.OnClickListener(){
-        public void onClick(View v) {
+    final View.OnClickListener BStartToggle_OnClickListener = new View.OnClickListener() {
+        public void onClick(View v){
             //place holder
             TStatusText.setText("Started");
             BStartToggle.setText("Breakfast");
+
         }
     };
 
@@ -65,12 +83,23 @@ public class frontpage extends Activity {
 
             int seekValue = progress + 1;
 
+            SimpleDateFormat dayFormat;
+
             if (seekValue == 1) {
                 TSeekVal.setText("Duration: " + seekValue + " hour");
             }
             else{
                 TSeekVal.setText("Duration: " + seekValue + " hours");
             }
+
+            futureCalendar = Calendar.getInstance();
+
+            dayFormat = new SimpleDateFormat(" EE");//For the day of the week.
+
+            futureCalendar.add(Calendar.HOUR_OF_DAY, seekValue);//Adds seekValue to the current hour of the day.
+            TEndHour.setText(futureHourFormat.format(futureCalendar.getTime()));
+            TDayText.setText(dayFormat.format(futureCalendar.getTime()));
+
         }
 
         @Override
@@ -83,15 +112,22 @@ public class frontpage extends Activity {
 
         }
     };
-
     public void upDateTime() {
         runOnUiThread(new Runnable() {
             public void run() {
                 try{
-                    Calendar myCalendar = Calendar.getInstance();
-                    SimpleDateFormat timeFormat;
-                    timeFormat = new SimpleDateFormat("HH:mm:ss");
-                    TCurrentClock.setText(timeFormat.format(myCalendar.getTime()));
+                    int previousHour =  currentCalendar.get(Calendar.HOUR_OF_DAY);
+
+                    currentCalendar = Calendar.getInstance();
+
+                    if (previousHour != currentCalendar.get(Calendar.HOUR_OF_DAY))
+                    {
+                        // This block of code checks if the hour has changed, if it has the futureTime gets incremented by 1.
+                        futureCalendar.add(Calendar.HOUR_OF_DAY, 1);
+                        TEndHour.setText(futureHourFormat.format(futureCalendar.getTime()));
+                    }
+                    TCurrentClock.setText(currentTimeFormat.format(currentCalendar.getTime()));
+                    TEndClock.setText(futureMinuteFormat.format(currentCalendar.getTime()));
                 }
                 catch (Exception e) {
 
@@ -101,10 +137,8 @@ public class frontpage extends Activity {
         });
     }
 
-
     class CountDownRunner implements Runnable{
-        //This thread runs every 1 second in the background which updates the text view in the doWork() method.
-        // @Override
+        //This thread runs every 1 second in the background which updates the text view in the upDateTime() method.
         public void run() {
             while(!Thread.currentThread().isInterrupted()){
                 try {
@@ -113,6 +147,7 @@ public class frontpage extends Activity {
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }catch(Exception e){
+                    Thread.currentThread().interrupt();
                 }
             }
         }
